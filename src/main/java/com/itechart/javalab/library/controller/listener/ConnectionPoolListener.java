520da13b46c617044ea0ceb2083542f8afbb105c
IrynaZazyba <lady.ira100@gmail.com>
2020-08-27 17:54:35 +0300

    Task#2 - replace Connection pool

--- src/main/java/com/itechart/javalab/library/controller/listener/ConnectionPoolListener.java
+++ src/main/java/com/itechart/javalab/library/controller/listener/ConnectionPoolListener.java
@@ -1,13 +1,13 @@
 package com.itechart.javalab.library.controller.listener;
 
 import com.itechart.javalab.library.dao.conn.ConnectionPool;
-import com.itechart.javalab.library.dao.conn.ConnectionPoolException;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.SQLException;
 import java.util.Properties;
 
 @WebListener
@@ -15,7 +15,6 @@ public class ConnectionPoolListener implements ServletContextListener {
 
     private final static String DB_PROPERTIES_FILE_NAME = "db";
 
-    private final static String DB_DRIVER = "db.driver";
     private final static String DB_URL = "db.url";
     private final static String DB_USER = "db.user";
     private final static String DB_PASSWORD = "db.password";
@@ -28,7 +27,6 @@ public class ConnectionPoolListener implements ServletContextListener {
     public void contextInitialized(ServletContextEvent sce) {
 
         Properties dbProperties = getDbProperties(sce);
-        String dbDriver = dbProperties.getProperty(DB_DRIVER);
         String dbUrl = dbProperties.getProperty(DB_URL);
         String dbUser = dbProperties.getProperty(DB_USER);
         String dbPassword = dbProperties.getProperty(DB_PASSWORD);
@@ -40,15 +38,16 @@ public class ConnectionPoolListener implements ServletContextListener {
         }
         connectionPool = ConnectionPool.getInstance();
         try {
-            connectionPool.initPoolData(dbDriver, dbUrl, dbUser, dbPassword, dbPoolSize);
-        } catch (ConnectionPoolException e) {
+            connectionPool.initPoolData(dbUrl, dbUser, dbPassword, dbPoolSize);
+        } catch (SQLException e) {
             //todo logger
         }
+
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        connectionPool.dispose();
+        connectionPool.closePool();
     }
 
 
